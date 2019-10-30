ebca7c4da464e8b161395c4adf22538e2b2be2b0
taiga <taiga912ar@gmail.com>
2019-10-30 17:22:49 +0900

    sampleDataの作成開始とttlの表示システムの作成

--- src/components/main/main_index.vue
+++ src/components/main/main_index.vue
@@ -1,27 +1,89 @@
 <template>
   <div class="mainIndex">
     <!-- <MasterNav></MasterNav> -->
+    <div class="headCont">
+      <div class="datalevelBox">
+        <h3>{{changeDataLevel()}}</h3>
+        <button v-if="getDataLevel()">back</button>
+      </div>
+    </div>
+    <ag-grid-vue class="masterGrid" :columnDefs="columnDefs" :rowData="rowData"></ag-grid-vue>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-// 
+import { Component, Prop, Vue } from "vue-property-decorator";
+//
 // import MasterNav from '@/components/common/nav.vue';
-
+import { AgGridVue } from "ag-grid-vue";
 @Component({
-  components:{
-    // MasterNav
+  components: {
+    AgGridVue
   }
 })
 export default class mainIndex extends Vue {
-    private test(){
-        this.$store.dispatch('changePageName','aaa');
-    }
-}
+  private test() {
+    this.$store.dispatch("changePageName", "aaa");
+  }
 
+  private dataleverName: string = "";
+
+  private columnDefs: any = null;
+  private rowData: any = null;
+
+  private namelevelArr:string[] = [];
+  private getDataLevel(){
+    return this.$store.getters.getDatalevelName; 
+  }
+
+  private idDataLevel(){
+    const namelevelArr:string = this.getDataLevel();
+    return namelevelArr.length > 0;
+  }
+
+  private changeDataLevel() {
+    this.namelevelArr = this.getDataLevel();
+    if(this.namelevelArr.length > 0){
+      let newName:string = "一覧 → ";
+      this.namelevelArr.forEach((name,i) =>{
+        newName += i == this.namelevelArr.length-1? name : name + " → ";
+      });
+      this.dataleverName = newName;
+      return newName;
+    }else{
+      this.dataleverName = 'ソリューション一覧'
+      return 'ソリューション一覧';
+    }
+  }
+
+  mounted(): void {
+    this.columnDefs = [
+      { headerName: "Make", field: "make" },
+      { headerName: "Model", field: "model" },
+      { headerName: "Price", field: "price" }
+    ];
+
+    this.rowData = [
+      { make: "Toyota", model: "Celica", price: 35000 },
+      { make: "Ford", model: "Mondeo", price: 32000 },
+      { make: "Porsche", model: "Boxter", price: 72000 }
+    ];
+  }
+}
 </script>
 
 <style scoped lang="scss">
 @import "@/assets/style/index.scss";
+.mainIndex {
+  background-color: $white;
+  box-sizing: border-box;
+  width: 100%;
+  padding-left: 4.7rem;
+}
+
+.masterGrid {
+  width: 100%;
+  height: 50vw;
+  // background-color: red;
+}
 </style>
--- src/store.ts
+++ src/store.ts
@@ -4,28 +4,191 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export interface pageState {
-  pageId:number;
-  pageName:string;
+  pageId: number;
+  pageName: string;
+}
+
+export interface dataSet {
+  id: number;
+  name: string;
+  place: string[];
+  target: string[];
+  usetech: string[];
+  text: string[];
+  img: any;
+  child: string[];
 }
 
 export default new Vuex.Store({
   state: {
-      pageId:0
-    
+    pageId: 0,
+    selectContent: 0,
+
+    masterData: [
+      {
+        id: 0,
+        name: 'デスクcloud',
+        place: ['All place'],
+        target: ['学生'],
+        usetech: [],
+        text: [],
+        img: null,
+        child: ['授業中名刺交換', 'グループトーク', 'プラグイン', 'ジェスチャー', '教科書表示', '感情サーモグラフィー', '出席管理', 'day todo', '場所チャット', '机模様替え'],
+      },{
+        id: 1,
+        name: 'aaa',
+        place: ['All place'],
+        target: ['aa'],
+        usetech: [],
+        text: [],
+        img: null,
+        child: ['aa', 'グループトーク', 'プラグイン', 'ジェスチャー', '教科書表示', '感情サーモグラフィー', '出席管理', 'day todo', '場所チャット', '机模様替え'],
+      },{
+        id: 2,
+        name: 'bb',
+        place: ['bb'],
+        target: ['bb'],
+        usetech: [],
+        text: [],
+        img: null,
+        child: ['bb', 'グループトーク', 'プラグイン', 'ジェスチャー', '教科書表示', '感情サーモグラフィー', '出席管理', 'day todo', '場所チャット', '机模様替え'],
+      },{
+        id: 3,
+        name: 'cc',
+        place: ['All place'],
+        target: ['学生'],
+        usetech: [],
+        text: [],
+        img: null,
+        child: ['cc', 'グループトーク', 'プラグイン', 'ジェスチャー', '教科書表示', '感情サーモグラフィー', '出席管理', 'day todo', '場所チャット', '机模様替え'],
+      },{
+        id: 4,
+        name: 'dd',
+        place: ['All place'],
+        target: ['dd'],
+        usetech: [],
+        text: [],
+        img: null,
+        child: ['dd', 'グループトーク', 'プラグイン', 'ジェスチャー', '教科書表示', '感情サーモグラフィー', '出席管理', 'day todo', '場所チャット', '机模様替え'],
+      },{
+        id: 5,
+        name: 'deed',
+        place: ['ee'],
+        target: ['学生'],
+        usetech: [],
+        text: [],
+        img: null,
+        child: ['ee', 'グループトーク', 'プラグイン', 'ジェスチャー', '教科書表示', '感情サーモグラフィー', '出席管理', 'day todo', '場所チャット', '机模様替え'],
+      },{
+        id: 6,
+        name: 'ff',
+        place: ['All place'],
+        target: ['学生'],
+        usetech: [],
+        text: [],
+        img: null,
+        child: ['授業中名刺交換', 'グループトーク', 'プラグイン', 'ジェスチャー', '教科書表示', '感情サーモグラフィー', '出席管理', 'day todo', '場所チャット', '机模様替え'],
+      }
+    ] as dataSet[],
+
+    showData: [
+      {
+        id: 0,
+        name: 'デスクcloud',
+        place: ['All place'],
+        target: ['学生'],
+        usetech: [],
+        text: [],
+        img: null,
+        child: ['授業中名刺交換', 'グループトーク', 'プラグイン', 'ジェスチャー', '教科書表示', '感情サーモグラフィー', '出席管理', 'day todo', '場所チャット', '机模様替え'],
+      },{
+        id: 1,
+        name: 'aaa',
+        place: ['All place'],
+        target: ['aa'],
+        usetech: [],
+        text: [],
+        img: null,
+        child: ['aa', 'グループトーク', 'プラグイン', 'ジェスチャー', '教科書表示', '感情サーモグラフィー', '出席管理', 'day todo', '場所チャット', '机模様替え'],
+      },{
+        id: 2,
+        name: 'bb',
+        place: ['bb'],
+        target: ['bb'],
+        usetech: [],
+        text: [],
+        img: null,
+        child: ['bb', 'グループトーク', 'プラグイン', 'ジェスチャー', '教科書表示', '感情サーモグラフィー', '出席管理', 'day todo', '場所チャット', '机模様替え'],
+      },{
+        id: 3,
+        name: 'cc',
+        place: ['All place'],
+        target: ['学生'],
+        usetech: [],
+        text: [],
+        img: null,
+        child: ['cc', 'グループトーク', 'プラグイン', 'ジェスチャー', '教科書表示', '感情サーモグラフィー', '出席管理', 'day todo', '場所チャット', '机模様替え'],
+      },{
+        id: 4,
+        name: 'dd',
+        place: ['All place'],
+        target: ['dd'],
+        usetech: [],
+        text: [],
+        img: null,
+        child: ['dd', 'グループトーク', 'プラグイン', 'ジェスチャー', '教科書表示', '感情サーモグラフィー', '出席管理', 'day todo', '場所チャット', '机模様替え'],
+      },{
+        id: 5,
+        name: 'deed',
+        place: ['ee'],
+        target: ['学生'],
+        usetech: [],
+        text: [],
+        img: null,
+        child: ['ee', 'グループトーク', 'プラグイン', 'ジェスチャー', '教科書表示', '感情サーモグラフィー', '出席管理', 'day todo', '場所チャット', '机模様替え'],
+      },{
+        id: 6,
+        name: 'ff',
+        place: ['All place'],
+        target: ['学生'],
+        usetech: [],
+        text: [],
+        img: null,
+        child: ['授業中名刺交換', 'グループトーク', 'プラグイン', 'ジェスチャー', '教科書表示', '感情サーモグラフィー', '出席管理', 'day todo', '場所チャット', '机模様替え'],
+      }
+    ] as dataSet[],    
+
+    datalevelName:['aa','bb']as string[],
+
   },
-  getters:{
-    getPageId(state){
+  getters: {
+    getPageId(state) {
       return state.pageId;
+    },
+    getMasterData(state){
+      return state.masterData;
+    },
+    getShowData(state){
+      return state.showData;
+    },
+    getDatalevelName(state){
+      return state.datalevelName;
     }
   },
   mutations: {
-    setPageName(state,pageId:number){
+    setPageId(state, pageId: number) {
       state.pageId = pageId;
+    },
+    setShowData(state, dataSet:dataSet[]){
+      state.showData = dataSet
     }
   },
   actions: {
-    changePageId({commit},pageId:number){
-      commit('setPageName',pageId);
+    changePageId({ commit }, pageId: number) {
+      commit('setPageId', pageId);
+    },
+    updateShowData({commit},dataSet:dataSet[]){
+      commit('setShowData', dataSet);
     }
   }
 })
